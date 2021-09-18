f0adb2b8f735c721c52ae25cf7cfecbca072264a
mouyan <12878752+mouyan@users.noreply.github.com>
2021-09-19 00:36:16 +0900

    first commit

---
+++ ClipboardViewer/ClipboardViewer/ClipboardController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+
+namespace ClipboardViewer
+{
+    /// <summary>
+    /// ClipBoardController
+    /// </summary>
+    internal class ClipboardController : IDisposable
+    {
+        private HWND hWnd;
+        /// <summary>
+        /// 更新時に呼ばれるイベントハンドラ
+        /// </summary>
+        internal event EventHandler ClipboardUpdated;
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="window">対象ウィンドウ</param>
+        internal unsafe ClipboardController(Window window)
+        {
+            // WPFでウィンドウプロシージャをフックする場合、
+            // 対象WindowからHwndSourceを取得し、それに対してフックを追加する
+            HwndSource source = (HwndSource)PresentationSource.FromVisual(window);
+            source.AddHook(WndProc);
+            // ハンドルを取得しリスナを追加
+            hWnd = new(source.Handle);
+            _ = PInvoke.AddClipboardFormatListener(hWnd);
+        }
+        /// <summary>
+        /// ウィンドウプロシージャ
+        /// </summary>
+        /// <param name="hwnd">ウィンドウハンドル</param>
+        /// <param name="message">メッセージコード</param>
+        /// <param name="wParam">w-パラメータ</param>
+        /// <param name="lParam">l-パラメータ</param>
+        /// <param name="handled"></param>
+        /// <seealso cref="https://docs.microsoft.com/ja-jp/dotnet/desktop/wpf/advanced/walkthrough-hosting-a-win32-control-in-wpf?view=netframeworkdesktop-4.8"/>
+        /// <returns></returns>
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            /// <summary>
+            /// ClipBpardの内容が変更された時
+            /// https://docs.microsoft.com/en-us/windows/win32/dataxchg/wm-clipboardupdate
+            if (msg == Constants.WM_CLIPBOARDUPDATE)
+            {
+                ClipboardUpdated?.Invoke(this, EventArgs.Empty);
+                handled = true;
+            }
+            return IntPtr.Zero;
+        }
+        /// <summary>
+        /// 破棄
+        /// </summary>
+        void IDisposable.Dispose() => _ = PInvoke.RemoveClipboardFormatListener(hWnd);
+    }
+}
---
+++ ClipboardViewer/ClipboardViewer/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+
+namespace ClipboardViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Win32との相互運用用のメソッド
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            // クリップボード操作用クラスを初期化
+            ClipboardController controller = new(this);
+            // 更新イベントが呼ばれたときにクリップボードのテキストを取得
+            controller.ClipboardUpdated += (_, __) =>
+            {
+                if (Clipboard.ContainsText())
+                {
+                    this.textBlock.Text = Clipboard.GetText();
+                }
+            };
+        }
+    }
+}
